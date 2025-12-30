35cd362d04458fb514fbb345422aace902ec0893
danipopp <popp.daniel16@icloud.com>
2025-12-30 23:28:16 +0100

    Maze load

--- Maze.py
+++ Maze.py
@@ -1,9 +1,23 @@
 class Maze:
-    def __init__(self):
-        pass
+    def __init__(self, filename):
+        self.grid = []
+        self.height = 0
+        self.width = 0
+        self.load_map(filename)
 
-    def load_map(self):
-        pass
+    def load_map(self, filename):
+        with open(filename,'r') as f:
+            lines = f.read().splitlines()
+
+        self.type = lines[0].split()[1]  # 'octile'
+        self.height = int(lines[1].split()[1])
+        self.width = int(lines[2].split()[1])
+        
+        # Read the actual map
+        map_lines = lines[4:]  # everything after 'map' line
+        self.grid = [list(line.strip()) for line in map_lines]
+
+        lines = lines
 
     def display(self):
         pass
---
+++ main.py
@@ -0,0 +1,3 @@
+from Maze import Maze
+
+maze = Maze('maze-map/maze512-1-0.map')
