dcfd517fd55b5f63e6467c3d61e44ac2e2faa259
danipopp <popp.daniel16@icloud.com>
2025-12-31 16:18:25 +0100

    define Functions and Init

--- Algorithem/BreathFirstSearch.py
+++ Algorithem/BreathFirstSearch.py
@@ -1,3 +1,9 @@
 class BFS:
-    def __init__(self):
+    def __init__(self, maze):
+        self.maze = maze
+
+    def find_neighbours(self, x, y):
+        pass
+
+    def solve(self, start, goal):
         pass
