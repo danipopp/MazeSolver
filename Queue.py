fb88b5a858db8d5013966a15d7f57137891277db
danipopp <popp.daniel16@icloud.com>
2025-12-31 13:21:59 +0100

    FIFO enque + deque + empty function

--- Queue.py
+++ Queue.py
@@ -1,3 +1,14 @@
 class FIFO:
     def __init__(self):
-        pass
+        self.iteams = []
+
+    def enqueue(self, item):
+        self.iteams.append(item)
+
+    def dequeue(self):
+        if not self.is_empty():
+            return self.iteams.pop(0)
+        return None
+
+    def is_empty(self):
+        return len(self.iteams) == 0
